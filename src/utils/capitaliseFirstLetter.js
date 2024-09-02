820e8c17938233d72ef43d24c826ec6316a44070
Kaustav Tarafdar <142386782+KausTarafdar@users.noreply.github.com>
2024-09-02 07:20:59 +0530

    Delete src directory

--- src/utils/capitaliseFirstLetter.js
+++
@@ -1,8 +0,0 @@
-/**
- * Makes the first letter uppercase of any string provided
- * @param {String} string - Takes a string.
- * @returns {String}
- */
-export default function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
